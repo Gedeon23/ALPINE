0fa74f2fba8db4554b0db2cbf79fed1577f89537
Gedeon Jahns <gedeon.jahns@gmail.com>
2025-06-17 07:22:47 +0200

    fix bug

--- aimclib/simple_example.cc
+++ aimclib/simple_example.cc
@@ -24,7 +24,7 @@ int main(int argc, char * argv[])
     std::cout << "input vec:\n";
     for (int i = 0; i < n_x; i++) {
         input[i] = (int8_t)rand();
-        std::cout << input[i] << ", ";
+        std::cout << static_cast<int>(input[i]) << ", ";
     }
     for (int i = 0; i < n_x; i++) {
         output[i] = (int8_t)rand();
@@ -33,21 +33,25 @@ int main(int argc, char * argv[])
     int8_t *  W1        = new int8_t[n_x*n_x];
     for (int i = 0; i < n_x*n_x; i++) {
         W1[i] = (int8_t)rand();
-        std::string delim = (i % n_x == 0) ? ", " : "\n";
-        std::cout << W1[i] << delim;
+        std::string delim = (i % n_x == 0) ? "\n" : ", ";
+        std::cout << static_cast<int>(W1[i]) << delim;
     }
 
     // Map weights to AIMC tile.
+    std::cout << "mapping matrix";
     mapMatrix(0, 0, n_x, n_x, W1);
 
     // Do inference.
     // Queue input for next inference in first layer.
+    std::cout << "queuing vector";
     queueVector(n_x, input);
     
     // Do MVM.
+    std::cout << "processing";
     aimcProcess();
     
     // Dequeue output from AIMC tile MVM.
+    std::cout << "dequeuing vector";
     dequeueVector(n_x, output);
 
     std::cout << "output vec:\n";
